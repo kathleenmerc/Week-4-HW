097c7ed59989576143990357ee672f9829d888af
Kathleen Mercado <kathleenmerc@gmail.com>
2022-10-24 19:16:01 -0400

    first commit, added parts 1-3

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ let quotes = [
   `I don't have friends. I have family.`,
   `It don't matter if you win by an inch or a mile. Winning's winning.`
 ];
+
+// Part 1
+// DOM's personal website title is a bit wordy. Write a JavaScript statement that selects the #main-title ID element. Remember there are a couple of ways to query id. Change the text of the title to something shorter.
+
+const mainTitleEl = document.querySelector("#main-title")
+mainTitleEl.textContent = "DOM TORETTO"
+
+// Part 2
+// Select the body and change the background-color to a new color of your choice.
+
+const bodyEl = document.querySelector("body")
+bodyEl.style.backgroundColor = "#2A52BE"
+
+// Part 3
+// Select DOM's Favorite Things list and remove the last list item.
+const favoriteThingsEl = document.querySelector("#favorite-things")
+favoriteThingsEl.removeChild(favoriteThingsEl.lastElementChild)
+console.log(favoriteThingsEl)
+
